ceec1ef8cd134502183e6721732640d00f67b987
Immanuel Mruk <immanuel.mruk@8mylez.com>
2024-08-26 14:08:23 +0200

    Implement Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,17 @@
 <template>
     <header>
         <div class="header-logo">
+            <img src="../assets/images/logo.png" alt="logo"
+                style="width: 180px; height: 180px;">
             <h1>lytex</h1>
         </div>
+
+        <nav class="header-nav">
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
     </header>
 </template>
