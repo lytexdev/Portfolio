3d949e0652dabd9c2a88b71682c49c0a3dcf2b23
Immanuel Mruk <immanuel.mruk@8mylez.com>
2024-09-10 16:03:00 +0200

    Add contact section

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,15 @@
 
   <main>
     <About />
+
+    <Contact />
   </main>
 </template>
 
 <script setup>
 import Header from './components/Header.vue'
 import About from './components/About.vue';
+import Contact from './components/Contact.vue';
 </script>
 
 <style lang="scss">
---
+++ src/components/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+    <section id="contact">
+        <h2>Kontakt</h2>
+        <div class="contact-info">
+            <div class="contact-item">
+                <p>E-Mail Adresse:</p>
+                <a href="mailto:immanuel@lytex.dev"
+                    title="Klicken um E-Mail an immanuel@lytex.dev zu senden">immanuel@lytex.dev</a>
+                <div class="popover">Schick mir eine E-Mail</div>
+            </div>
+            <div class="contact-item">
+                <p>Discord:</p>
+                <a href="#contact">_lytex_</a>
+                <div class="popover">Füg mich auf Discord hinzu</div>
+            </div>
+            <div class="contact-item">
+                <p>Instagram:</p>
+                <a href="https://www.instagram.com/ximmanuelm" target="_blank">@ximmanuelm</a>
+                <div class="popover">Erreiche mich über Instagram</div>
+            </div>
+            <div class="contact-item">
+                <p>Twitter:</p>
+                <a href="https://x.com/ximmanuelm" target="_blank">@ximmanuelm</a>
+                <div class="popover">Erreiche mich über Twitter</div>
+            </div>
+        </div>
+    </section>
+    <hr>
+</template>
